9255e720e676f633a6fec7bc0f838175160b6708
avarusjose <joseaizaguirrep@gmail.com>
2024-10-24 11:24:15 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const share = document.getElementById("section_share");
+const button = document.getElementById("svg_cont");
+
+button.addEventListener("click", () => {
+  share.classList.toggle("hidden");
+});
